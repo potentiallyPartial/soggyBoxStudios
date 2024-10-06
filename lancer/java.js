d614ba17668143bf08e88cd4d8917ecf2590b5b4
unknown <rianaddy@gmail.com>
2024-10-06 02:10:02 -0400

    adding Lancer Page

---
+++ lancer/java.js
@@ -0,0 +1,36 @@
+
+var value = 0;
+
+var Paragraph = document.getElementsByClassName("o1");
+
+function input(num){
+
+
+if (num == 5 & value == 0){
+    value = value + 1;
+
+}
+
+else if (num == 3 & value == 1){
+    value = value + 1;
+
+}
+
+else if (num == 1 & value == 2){
+    value = value + 1;
+
+}
+
+else if (num == 8 & value == 3){
+    value = value + 1;
+    console.log(value);
+}
+
+else if (num == 6 & value == 4){
+    Paragraph[0].innerHTML = "Pillers cut from the immutable cloth hold forces deprecated to show the unliving h̶̛͈̼̤͓͓̪̖̻̱̩̳̿̋̂̄͆̊̓̔̃͂͘̕͜e̴̛̗̖̰͍̬͔͖̤̩͗͋̇a̴͈͈̼̰͓͈̹̜͊̓͘̕ŗ̴̢̩̺̭̰̪͇͖͎̅̒̆́̾͠ẗ̴̡̀̈  <br><pre>             ○<br>          ○  Σ  ○<br>         ○ Σ ☼ Σ ○<br>          ○  Σ  ○<br>             ○</pre>"
+}
+
+console.log(value);
+console.log(num);
+
+}
